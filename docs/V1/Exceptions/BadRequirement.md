2137df022deead1362d0f8d9d67be31074a7a244
Stuart Herbert <stuart@stuartherbert.com>
2016-04-24 21:46:45 +0100

    New: added BadRequirement exception

---
+++ docs/V1/Exceptions/BadRequirement.md
@@ -0,0 +1,152 @@
+---
+currentSection: v1
+currentItem: exceptions
+pageflow_prev_url: index.html
+pageflow_prev_text: Exceptions List
+pageflow_next_url: BadRequirementData.html
+pageflow_next_text: BadRequirementData Class
+---
+
+# BadRequirement
+
+<div class="callout warning" markdown="1">
+Not yet in a tagged release
+</div>
+
+## Description
+
+`BadRequirement` is an exception. It is thrown when one of the requirements passed into `RequireAllOf` or `RequireAnyOneOf` isn't callable.
+
+## Public Interface
+
+`BadRequirement` has the following public interface:
+
+```php
+// our base class and interface(s)
+use GanbaroDigital\ExceptionHelpers\V1\BaseExceptions\ParameterisedException;
+use GanbaroDigital\HttpStatus\Specifications\HttpStatusProvider;
+
+// return types from our method(s)
+use GanbaroDigital\HttpStatus\StatusValues\RequestError\UnprocessableEntityStatus;
+
+// how to import
+use GanbaroDigital\Defensive\V1\Exceptions\BadRequirement;
+
+class BadRequirement
+  extends ParameterisedException
+  implements DefensiveException, HttpStatusProvider
+{
+    // we map onto HTTP 422
+    use UnprocessableEntityStatusProvider;
+
+    /**
+     * create a new exception from the requirement that has been
+     * rejected
+     *
+     * @param  mixed $requirement
+     *         the requirement that has been rejected
+     * @param  array|null $callerFilter
+     *         a list of classnames or partial namespaces to avoid
+     *         if null, we use FilterCodeCaller::$DEFAULT_PARTIALS
+     * @return BadRequirements
+     */
+    public static function newFromRequirement($requirement, $callerFilter = null);
+
+    /**
+     * which HTTP status code do we map onto?
+     * @return UnprocessableEntityStatusProvider
+     */
+    public function getHttpStatus();
+}
+
+```
+
+## How To Use
+
+### Creating Exceptions To Throw
+
+Call `BadRequirement::newFromRequirement()` to create a new throwable exception:
+
+```php
+throw BadRequirement::newFromRequirement([]);
+```
+
+### Catching The Exception
+
+`BadRequirement` extends or implements a rich set of classes and interfaces. You can use any of these to catch thrown exceptions.
+
+```php
+// example 1: we catch only BadRequirement exceptions
+use GanbaroDigital\Defensive\V1\Exceptions\BadRequirement;
+
+try {
+    throw BadRequirement::newFromRequirement([]);
+}
+catch(BadRequirement $e) {
+    // ...
+}
+```
+
+```php
+// example 2: catch all exceptions thrown by the Defensive Library
+use GanbaroDigital\Defensive\V1\Exceptions\BadRequirement;
+use GanbaroDigital\Defensive\V1\Exceptions\DefensiveException;
+
+try {
+    throw BadRequirement::newFromRequirement([]);
+}
+catch(DefensiveException $e) {
+    // ...
+}
+```
+
+```php
+// example 3: catch all exceptions where there was a problem with the
+// parameter(s) passed into the method
+use GanbaroDigital\Defensive\V1\Exceptions\BadRequirement;
+use GanbaroDigital\HttpStatus\Specifications\RequestError;
+
+try {
+    throw BadRequirement::newFromRequirement([]);
+}
+catch(RequestError $e) {
+    $httpStatus = $e->getHttpStatus();
+    // ...
+}
+```
+
+```php
+// example 4: catch all exceptions that map onto a HTTP status
+use GanbaroDigital\Defensive\V1\Exceptions\BadRequirement;
+use GanbaroDigital\HttpStatus\Specifications\HttpStatusProvider;
+
+try {
+    throw BadRequirement::newFromRequirement([]);
+}
+catch(HttpStatusProvider $e) {
+    $httpStatus = $e->getHttpStatus();
+    // ...
+}
+```
+
+```php
+// example 5: catch all runtime exceptions
+use GanbaroDigital\Defensive\V1\Exceptions\BadRequirement;
+use RuntimeException;
+
+try {
+    throw BadRequirement::newFromRequirement([]);
+}
+catch(RuntimeException $e) {
+    // ...
+}
+```
+
+## Notes
+
+None at this time.
+
+## See Also
+
+* [`ParameterisedException` class](http://ganbarodigital.github.io/php-mv-exception-helpers/V1/BaseExceptions/ParameterisedException.html)
+* [`HttpStatusProvider` interface](http://ganbarodigital.github.io/php-http-status/httpStatusProviders.html)
---
+++ src/V1/Exceptions/BadRequirement.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   Defensive/V1/Exceptions
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://ganbarodigital.github.io/php-mv-defensive
+ */
+
+namespace GanbaroDigital\Defensive\V1\Exceptions;
+
+use GanbaroDigital\ExceptionHelpers\V1\BaseExceptions\ParameterisedException;
+use GanbaroDigital\ExceptionHelpers\V1\Callers\Filters\FilterBacktraceForTwoCodeCallers;
+use GanbaroDigital\ExceptionHelpers\V1\Callers\Filters\FilterCodeCaller;
+use GanbaroDigital\HttpStatus\Specifications\HttpStatusProvider;
+use GanbaroDigital\HttpStatus\StatusProviders\RequestError\UnprocessableEntityStatusProvider;
+use GanbaroDigital\MissingBits\TypeInspectors\GetPrintableType;
+
+class BadRequirement extends ParameterisedException implements DefensiveException, HttpStatusProvider
+{
+    // we map onto HTTP 422
+    use UnprocessableEntityStatusProvider;
+
+    /**
+     * create a new exception from the requirement that has been
+     * rejected
+     *
+     * @param  mixed $requirement
+     *         the requirement that has been rejected
+     * @param  array|null $callerFilter
+     *         a list of classnames or partial namespaces to avoid
+     *         if null, we use FilterCodeCaller::$DEFAULT_PARTIALS
+     * @return BadRequirements
+     */
+    public static function newFromRequirement($requirement, $callerFilter = null)
+    {
+        // do we need to provide a filter?
+        if (!is_array($callerFilter)) {
+            $callerFilter = FilterCodeCaller::$DEFAULT_PARTIALS;
+        }
+
+        // who called us?
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $callers = FilterBacktraceForTwoCodeCallers::from($trace, $callerFilter);
+
+        // what is the bad data we've had?
+        $type = GetPrintableType::of($requirement);
+
+        // put it all together
+        $exceptionData = [
+            "thrownBy" => $callers[0],
+            "thrownByName" => $callers[0]->getCaller(),
+            "caller" => $callers[1],
+            "callerName" => $callers[1]->getCaller(),
+            "badRequirement" => $requirement,
+            "badRequirementType" => $type,
+        ];
+        $msg = "Bad requirement passed into %thrownByName\$s by %callerName\$s; must be callable";
+
+        // all done
+        return new static($msg, $exceptionData);
+    }
+}
